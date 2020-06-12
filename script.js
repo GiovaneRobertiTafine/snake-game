b93a8dc24f62fdf906ce7f5ec2628e8ec87a08f5
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2020-06-12 15:04:09 -0300

    Add limit in canvas for snake

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ function createSnake() {
 // Escutando o evento do teclado
 document.addEventListener('keydown', update);
 
-// prettier-ignore
-function update (event) {
+function update(event) {
     if (event.keyCode === 37 && direction !== 'right') direction = 'left';
     if (event.keyCode === 38 && direction !== 'down') direction = 'up';
     if (event.keyCode === 39 && direction !== 'left') direction = 'right';
@@ -41,6 +40,12 @@ function update (event) {
 
 // Iniciando o jogo
 function initialGame() {
+    // Verificando se o snake passou da tela
+    if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+    if (snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+    if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
+
     createBG();
     createSnake();
 
